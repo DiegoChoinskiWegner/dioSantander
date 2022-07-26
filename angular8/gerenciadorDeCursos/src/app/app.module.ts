34b8ffcb02c678c035b9639b41c589ded9d1b57f
Diego Choinski Wegner <102632737+DiegoChoinskiWegner@users.noreply.github.com>
2022-07-26 15:31:21 -0300

    Add files via upload

---
+++ angular8/gerenciadorDeCursos/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CourseListComponent } from './courses/course-list.component';
+import { OnInit } from '@angular/core';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './404/error404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '**', component: Error404Component
+      },
+    ])
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
